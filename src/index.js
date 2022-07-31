8063080357ca06128675dcd1314ebbb63605054b
AndreySmekhunov <105367492+AndreySmekhunov@users.noreply.github.com>
2022-07-31 16:15:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let i = 1;
+  let count = 0;
+  while (i < str.length) {
+      count = 0;  
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if (str[i-1] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]) {
+          str = str.slice(0,i-1) + str.slice(i+1);
+          count += 1;
+          }  
+      }
+      if (count === 0) i += 1
+        else i = 0;
+      if (str.length === 1) return false;
+  
+      }
+      if (str.length === 0) return true 
+      else return false;
+  }
